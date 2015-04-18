229062be82f089b61c2f86ed82fa23fdb17db59d
thismali <mali@mail.umkc.edu>
2015-04-17 21:45:38 -0500

    register wurfl

--- RestService/Backup/App_Start/AnaysisStart.cs
+++ RestService/Backup/App_Start/AnaysisStart.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace RestService
+namespace RestService.App_Start
 {
     public class AnaysisStart
     {
-        public static WURFL.IWURFLManager wurflContainer;
+        
     }
 }
--- RestService/Backup/Global.asax.cs
+++ RestService/Backup/Global.asax.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
-using WURFL;
-using WURFL.Aspnet.Extensions.Config;
 
 namespace RestService
 {
     public class Global : System.Web.HttpApplication
     {
-        public const String WurflDataFilePath = "~/App_Data/wurfl-latest.zip";
-       
+
         protected void Application_Start(object sender, EventArgs e)
         {
 
-            var wurflDataFile = HttpContext.Current.Server.MapPath(WurflDataFilePath);
-            
-            
-            var configurer = new WURFL.Config.InMemoryConfigurer()
-            .MainFile(wurflDataFile);
-             AnaysisStart.wurflContainer = WURFLManagerBuilder.Build(configurer);
+            registerWurfl();
 
         }
 
